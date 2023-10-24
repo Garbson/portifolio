daeaf5254f46d8c9fb7cdaba15246f0e01c46a3b
Garbson <garbsonsouza2602@gmail.com>
2023-10-24 16:52:47 -0500

    mundança total

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,52 @@
 <template>
-  <div class="body">
-    <div class="apresentacao">
-      <img class="foto" src="./assets/img/logo.jpeg" alt="" />
-      <div class="redes">
-        <a href="https://www.instagram.com/dev_garbson/"><img src="./assets/img/instagram-2-1-logo-svgrepo-com.svg" alt="instagram" /></a>
-        <a href="https://github.com/Garbson"><img src="./assets/img/github-142-svgrepo-com.svg" alt="Github" /></a>
-        <a href="https://www.linkedin.com/in/garbson-souza-santos-0744a825a/">
-          <img src="./assets/img/linkedin-svgrepo-com.svg" alt="linkedli" />
-        </a>
+  <div class="body bg-gradient-to-b from-blue-500 to-teal-600">
+    <div class="apresentacao flex items-center justify-start ml-32">
+      <img class="foto w-70 h-96 rounded-full" src="./assets/img/logo.png" alt="Minha Foto" />
+      <div class="ml-8">
+        <h2 class="text-5xl font-semibold text-white">
+          Olá, sou Garbson!
+          <img src="../src/assets/img/programador.svg" class="w-12 h-12 inline-block ml-2" alt="" />
+        </h2>
+        <p class="text-xl text-white mt-4">Desenvolvedor Front-End Brasileiro.</p>
       </div>
-      <div class="sobre">
-        <p>Olá, meu nome é</p>
-        <h1>Garbson Souza</h1>
-        <h1>Sou um desenvolvedor FRONT-END</h1>
-        <p>
-          Sou apaixonado por programação e desafios,
-          <br />
-          e estou sempre em busca de conhecimento.
+    </div>
+
+    <div class="redes flex justify-center space-x-8">
+      <a href="https://www.instagram.com/dev_garbson/" class="text-white hover:text-blue-300">
+        <img src="./assets/img/instagram-2-1-logo-svgrepo-com.svg" alt="Instagram" class="w-12 h-12" />
+      </a>
+      <a href="https://github.com/Garbson" class="text-white hover:text-blue-300">
+        <img src="./assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-12 h-12" />
+      </a>
+      <a href="https://www.linkedin.com/in/garbson-souza-santos-0744a825a" class="text-white hover:text-blue-300">
+        <img src="./assets/img/linkedin-svgrepo-com.svg" alt="LinkedIn" class="w-12 h-12" />
+      </a>
+    </div>
+
+    <div class="biografia bg-blue-900 text-white p-8 mx-4 md:mx-32 my-4 rounded-lg flex">
+      <div class="w-1/2">
+        <img src="../public/biografia.jpg" alt="" class="w-full rounded-lg" />
+        <!-- Aqui você pode adicionar uma imagem ao lado do texto da biografia -->
+      </div>
+      <div class="w-1/2 ml-4">
+        <p class="text-2xl font-semibold mb-4">Biografia</p>
+        <p class="text-lg">
+          Olá, sou Garbson, um desenvolvedor front-end brasileiro. Desde os 16 anos, sou apaixonado por programação e atualmente estudo
+          Sistemas de Informação na Universidade Federal do Acre.
+        </p>
+        <p class="text-lg mt-4">
+          Nasci e cresci na região norte do Brasil e cursei o ensino médio no Colégio Militar. Sou um desenvolvedor comunicativo, proativo,
+          sempre disposto a abraçar novos desafios.
+        </p>
+        <p class="text-lg mt-4">
+          Além disso, sou dedicado e sempre interessado em aprender coisas novas, resolver problemas e enfrentar desafios complexos.
         </p>
       </div>
     </div>
 
-    <div class="container">
-      <div class="con">
-        <BaseCard
-          title="Gerenciador de tarefas"
-          description="Gerenciador de tarefas, metas e calendario"
-          img="gerenciador.png"
-          vue
-          tailwind
-          javascript
-          github="https://github.com/Garbson/GER"
-          link="https://ger-8tm.pages.dev/"
-        />
-
-        <BaseCard
-          title="Brasileiro.ninja"
-          description="Informações nacionais como ISBN, CEP, DDD..."
-          img="brasileiroo.jpeg"
-          vue
-          css
-          javascript
-          github="https://github.com/arnonrdp/Brasileiro-Ninja"
-          link="https://brasileiro.ninja/"
-        />
-
-        <BaseCard
-          title="Portifolio"
-          description="Site com projetos que construí e mantenho ativo"
-          img="portfolio.png"
-          vue
-          css
-          javascript
-          github="https://github.com/Garbson/portifolio"
-          link="https://portifolio-by1.pages.dev"
-        />
-
-        <BaseCard
-          title="Bitcoin"
-          description="Site com o valor em tempo real de moedas digitais"
-          img="bitcoin.jpeg"
-          html
-          css
-          javascript
-          github="https://github.com/Garbson/bitcoin"
-          link="https://bitcoin-27r.pages.dev/"
-        />
-
-        <BaseCard
-          title="Temperature"
-          description="Site feito para ver a temperatura de qualquel cidade"
-          img="temperatures.jpeg"
-          html
-          css
-          javascript
-          github="https://github.com/Garbson/projeto-site-pra-ver-a-temperatura"
-          link="https://temperature-9ta.pages.dev/"
-        />
-
-        <BaseCard
-          title="Conversor"
-          description="Conversor para o cambio de moedas do mundo"
-          img="Conversor.jpeg"
-          html
-          css
-          javascript
-          github="https://github.com/Garbson/Currency-Converter"
-          link="https://currency-converter-cgn.pages.dev/"
-        />
+    <div class="container mx-auto">
+      <div class="con flex flex-wrap justify-center gap-5 md:gap-8 mt-10">
+        <BaseCard></BaseCard>
       </div>
     </div>
   </div>
@@ -96,198 +55,3 @@
 <script setup>
 import BaseCard from './components/BaseCard.vue'
 </script>
-
-<style>
-body {
-  background: rgb(0, 0, 0);
-  background: linear-gradient(0deg, rgba(0, 0, 0, 1) 60%, rgba(118, 10, 10, 1) 100%);
-  object-fit: contain;
-  margin: 0;
-  padding: 0;
-  font-family: 'Oswald', sans-serif;
-  font-weight: bold;
-}
-
-header {
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  margin-top: 20px;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* Center the header itself horizontally */
-}
-
-/* .meu-nome {
-  margin-left: 10px;
-  padding: 10px;
-} */
-.apresentacao {
-  display: flex;
-  flex-direction: column; /* Altere para uma coluna */
-  align-items: center; /* Centralize horizontalmente */
-}
-
-.foto-container {
-  display: flex;
-  justify-content: center; /* Centralize horizontalmente */
-  align-items: center; /* Centralize verticalmente */
-}
-
-.foto {
-  width: 400px;
-  height: auto;
-  border-radius: 50%;
-  margin-top: 20px;
-}
-
-.redes {
-  margin-top: 10px;
-  display: flex;
-}
-
-.redes a {
-  display: flex;
-
-  align-items: center;
-  margin-bottom: 10px; /* Adicione espaço entre os ícones */
-}
-
-.redes img {
-  width: 30px;
-  height: auto;
-  margin-right: 10px;
-  border-radius: 50%;
-}
-
-.sobre {
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-  margin-bottom: 50px;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-right: 20px;
-  font-size: 20px;
-  color: #ffffff;
-  font-family: 'Oswald', sans-serif;
-}
-
-.meu-nome {
-  margin-left: 50px;
-}
-
-.descricao {
-  font-family: math;
-  margin-left: 100px;
-  font-size: 17px;
-}
-
-.portfolio {
-  color: #fffdfd;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-  height: 100px;
-  margin-top: 15px;
-}
-
-.div-content {
-  text-align: center;
-}
-
-.container {
-  margin: 0 auto;
-
-  max-width: 100%;
-
-  width: 100%;
-
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: flex-start;
-}
-
-.con {
-  max-width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-  flex-direction: row;
-  margin: 0 40px; /* Adjust this margin for better spacing on small screens */
-}
-@media screen and (max-width: 800px) {
-  .apresentacao {
-    /* coloque aqui os estilos que você quer aplicar */
-    flex-direction: column;
-    align-items: center;
-  }
-  .foto {
-    width: 50%;
-  }
-  .sobre {
-    text-align: center;
-  }
-  .redes {
-    display: flex;
-    justify-content: center;
-  }
-}
-
-@media screen and (max-width: 700px) {
-  .con {
-    /* Stack cards vertically */
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-  }
-
-  .con .BaseCard {
-    width: 100%; /* Make each card take full width on smaller screens */
-    margin-bottom: 20px; /* Add some space between stacked cards */
-  }
-}
-@media screen and (max-width: 700px) {
-  .apresentacao {
-    flex-direction: column;
-    align-items: center;
-    margin-left: 0;
-  }
-
-  .foto {
-    width: 70%;
-    max-width: 300px;
-    margin-left: 0; /* Remova a margem da esquerda */
-    margin-right: 0; /* Remova a margem da direita */
-  }
-
-  .sobre {
-    text-align: center;
-    margin-top: 20px;
-  }
-
-  .redes {
-    margin-top: 20px;
-  }
-
-  .redes img {
-    margin-bottom: 10px;
-  }
-
-  .portfolio {
-    margin-top: 20px;
-  }
-
-  .container {
-    margin: 0;
-  }
-
-  .con {
-    margin: 0;
-  }
-}
-</style>
--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,150 +1,94 @@
 <template>
-  <div class="projeto">
-    <a :href="link" target="_blank">
-      <img :src="img" :alt="title" />
-      <h3 class="titulo">{{ title }}</h3>
-      <p class="description">{{ description }}</p>
-      <div class="linguaguens-projeto">
-        <div>
-          <img v-if="html" src="@/assets/img/html.svg" alt="HTML" />
-          <img v-if="vue" src="@/assets/img/vue-svgrepo-com.svg" alt="" />
-          <img v-if="css" src="@/assets/img/css-3-svgrepo-com.svg" alt="CSS" />
-          <img v-if="tailwind" src="@/assets/img/tailwind.svg" alt="" />
-          <img
-            v-if="javascript"
-            src="@/assets/img/kisspng-javascript-programmer-node-js-web-application-vector-markup-language-5b173fc201e143.7543356315282503060077.png"
-            alt="JavaScript"
-          />
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div
+      v-for="project in projects"
+      :key="project.title"
+      class="projeto border border-white rounded-lg overflow-hidden bg-gradient-to-tl from-blue-400 to-blue-600 text-white"
+    >
+      <a :href="project.link" target="_blank" class="flex flex-col h-full w-full">
+        <img :src="project.img" :alt="project.title" class="w-full h-40 object-cover rounded-t-lg cursor-pointer" />
+        <div class="p-4">
+          <h3 class="titulo text-lg font-semibold mt-2">{{ project.title }}</h3>
+          <p class="description text-sm mt-2">{{ project.description }}</p>
         </div>
-        <a :href="github"><img src="@/assets/img/github-142-svgrepo-com.svg" alt="" /></a>
-      </div>
-    </a>
+        <div class="linguagens-projeto flex justify-between items-center p-4 border-t border-white">
+          <div class="flex space-x-2">
+            <img v-if="project.html" src="@/assets/img/html.svg" alt="HTML" class="w-6 h-6" />
+            <img v-if="project.vue" src="@/assets/img/vue-svgrepo-com.svg" alt="Vue" class="w-6 h-6" />
+            <img v-if="project.css" src="@/assets/img/css-3-svgrepo-com.svg" alt="CSS" class="w-6 h-6" />
+            <img v-if="project.tailwind" src="@/assets/img/tailwind.svg" alt="Tailwind" class="w-6 h-6" />
+            <img v-if="project.javascript" src="@/assets/img/javascript.svg.png" alt="JavaScript" class="w-6 h-6" />
+          </div>
+          <a :href="project.github">
+            <img src="@/assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-6 h-6" />
+          </a>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps({
-  title: String,
-  description: String,
-  img: String,
-  html: Boolean,
-  css: Boolean,
-  javascript: Boolean,
-  vue: Boolean,
-  tailwind: Boolean,
-  link: String,
-  github: String
-})
+const projects = [
+  {
+    title: 'Gerenciador de tarefas',
+    description: 'Gerenciador de tarefas, metas e calendário',
+    img: 'gerenciador.png',
+    vue: true,
+    tailwind: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/GER',
+    link: 'https://ger-8tm.pages.dev/'
+  },
+  {
+    title: 'Brasileiro.ninja',
+    description: 'Informações nacionais como ISBN, CEP, DDD...',
+    img: 'brasileiroo.jpeg',
+    vue: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/arnonrdp/Brasileiro-Ninja',
+    link: 'https://brasileiro.ninja/'
+  },
+  {
+    title: 'Portfólio',
+    description: 'Site com projetos que construí e mantenho ativo',
+    img: 'portfolio.png',
+    vue: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/portifolio',
+    link: 'https://portifolio-by1.pages.dev'
+  },
+  {
+    title: 'Bitcoin',
+    description: 'Site com o valor em tempo real de moedas digitais',
+    img: 'bitcoin.jpeg',
+    html: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/bitcoin',
+    link: 'https://bitcoin-27r.pages.dev/'
+  },
+  {
+    title: 'Temperature',
+    description: 'Site feito para ver a temperatura de qualquer cidade',
+    img: 'temperatures.jpeg',
+    html: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/projeto-site-pra-ver-a-temperatura',
+    link: 'https://temperature-9ta.pages.dev/'
+  },
+  {
+    title: 'Conversor',
+    description: 'Conversor para o câmbio de moedas do mundo',
+    img: 'Conversor.jpeg',
+    html: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/Currency-Converter',
+    link: 'https://currency-converter-cgn.pages.dev/'
+  }
+]
 </script>
-
-<style scoped>
-.projeto {
-  margin: 15px;
-  border: 2px solid #fff;
-  border-radius: 10px;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  width: calc(33% - 30px);
-  height: 100%;
-}
-
-.description {
-  color: aliceblue;
-}
-
-.projeto img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 10px;
-  cursor: pointer;
-}
-
-/* To center the content of each project (text) */
-.projeto a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-
-  flex-grow: 1;
-}
-
-.linguaguens-projeto {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-  width: 100%;
-  padding: 4px;
-}
-
-.linguaguens-projeto img {
-  width: 30px !important;
-  height: 100% !important;
-  /* margin-right: 5px; */
-}
-
-.linguaguens-projeto img:last-child {
-  margin-left: auto;
-  margin-right: 0;
-}
-
-.projeto img {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  border-radius: 10px;
-  cursor: pointer;
-}
-.titulo {
-  font-weight: bolder;
-  color: #fff;
-}
-
-/* To center the content of each project (text) */
-.projeto a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  background-color: #3a0808;
-}
-
-.linguaguens-projeto {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-  width: 100%;
-  padding: 4px;
-}
-
-.linguaguens-projeto img {
-  width: 30px !important;
-  height: 100% !important;
-  /* margin-right: 5px; */
-}
-
-.linguaguens-projeto img:last-child {
-  margin-left: auto;
-  margin-right: 0;
-}
-
-@media (max-width: 900px) {
-  .projeto {
-    width: calc(50% - 30px);
-    height: 100%;
-  }
-}
-
-@media (max-width: 700px) {
-  .projeto {
-    width: 100%;
-    height: 100%;
-  }
-}
-</style>
