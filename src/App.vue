980a99689ed1906ff1aa14969ac30489e41c2d71
Garbson <garbsonsouza2602@gmail.com>
2024-07-03 14:54:38 -0300

    novos projetos adcionados

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Apresentação -->
     <div class="md:w-[72%] mx-auto">
       <div class="apresentacao flex flex-col items-center md:flex-row justify-left md:w-[100%]">
-        <img class="foto w-64 h-82 md:w-70 md:h-70 rounded-full" src="./assets/img/ddd.png" alt="Minha Foto" />
+        <img class="foto w-64 h-82 md:w-70 md:h-70 rounded-full" src="profile.png" alt="Minha Foto" />
         <div class="bb md:text-left">
           <h2 class="text-3xl md:text-5xl font-semibold text-white">Olá, sou Garbson Souza!</h2>
           <p class="text-2xl text-white mt-2 md:mt-4">Desenvolvedor Front-End</p>
@@ -15,13 +15,20 @@
     <div class="biografia w-full md:w-[72%] mx-auto bg-blue-900 text-white p-5 rounded-lg flex flex-col items-center justify-center mt-5">
       <h1 class="text-2xl md:text-3xl font-bold mb-4">Sobre mim</h1>
       <p class="text-base md:text-lg">
-        Olá, sou Garbson, um desenvolvedor front-end brasileiro. Desde os 16 anos, sou apaixonado por programação e atualmente estudo
-        Sistemas de Informação na Universidade Federal do Acre.
+        👋 Olá, sou Garbson Souza Santos, um desenvolvedor front-end apaixonado por criar interfaces de usuário intuitivas e responsivas.
+        Com uma sólida base em tecnologias modernas, Vue.js, Quasar, Node.js, Nuxt.js, Vuex e Pinia, transformo ideias em experiências
+        digitais excepcionais. 💡
         <br />
-        Nasci e cresci na região norte do Brasil e cursei o ensino médio no Colégio Militar. Sou um desenvolvedor comunicativo, competitivo
-        e proativo, sempre disposto a abraçar novos desafios.
+        Desde os 16 anos, tenho me aprofundado no universo da programação, acumulando uma vasta experiência em projetos freelancer que
+        abrangem diferentes setores e geografias. Já trabalhei com uma empresa americana, desenvolvendo soluções inovadoras, e colaborei com
+        empresas de biotecnologia, além de um hotel em Cusco, no Peru, sempre focado em entregar produtos de alta qualidade e valor
+        agregado. 🌍 Atualmente, estou contribuindo como freelancer na Hellenic Technologies, uma empresa grega, onde continuo a aplicar e
+        expandir minhas competências.
         <br />
-        Além disso, sou dedicado e sempre interessado em aprender coisas novas, resolver problemas e enfrentar desafios complexos. 💪
+        Minha expertise inclui o consumo de APIs para a criação de aplicações web robustas e responsivas, garantindo uma performance
+        otimizada e uma experiência de usuário (UX) de excelência. 🚀 Além da minha experiência prática, estou cursando Sistemas de
+        Informação na Universidade Federal do Acre, o que me permite alinhar conhecimentos teóricos e práticos, mantendo-me atualizado com
+        as últimas tendências e inovações do mercado de tecnologia. 🎓
       </p>
       <div class="redes flex items-center mt-3">
         <a href="https://api.whatsapp.com/send?phone=5568992490198" class="text-white hover:text-blue-300">
@@ -33,7 +40,7 @@
         <a href="https://github.com/Garbson" class="text-white hover:text-blue-300">
           <img src="./assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-8 h-8 mr-4" />
         </a>
-        <a href="https://www.linkedin.com/in/garbson-souza-santos-0744a825a" class="text-white hover:text-blue-300">
+        <a href="https://www.linkedin.com/in/garbson-souza-0744a825a/" class="text-white hover:text-blue-300">
           <img src="./assets/img/linkedin-svgrepo-com.svg" alt="LinkedIn" class="w-9 h-9" />
         </a>
       </div>
--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -4,7 +4,7 @@
       <div
         v-for="project in projects"
         :key="project.title"
-        class="projeto shadow-md rounded-lg bg-gradient-to-tl from-blue-800 to-blue-900 text-white"
+        class="projeto shadow-md rounded-lg bg-gradient-to-tl from-blue-800 to-blue-900 text-white transform transition-transform duration-300 hover:scale-105"
       >
         <a :href="project.link" target="_blank" class="flex flex-col h-full w-full">
           <img
@@ -22,17 +22,18 @@
           </div>
           <div class="linguagens-projeto flex justify-between p-4 border-black">
             <div class="flex space-x-2 bottom-0">
-              <img v-if="project.firebase" src="@/assets/img/firebase.svg" alt="Firebase" class="w-6 h-6" :title="'Firebase'" />
-              <img v-if="project.html" src="@/assets/img/html.svg" alt="HTML" class="w-6 h-6" :title="'HTML'" />
-              <img v-if="project.vue" src="@/assets/img/vue-svgrepo-com.svg" alt="Vue" class="w-6 h-6" :title="'Vue.js'" />
-              <img v-if="project.css" src="@/assets/img/css-3-svgrepo-com.svg" alt="CSS" class="w-6 h-6" :title="'CSS'" />
-              <img v-if="project.tailwind" src="@/assets/img/tailwind.svg" alt="Tailwind" class="w-6 h-6" :title="'Tailwind'" />
-              <img v-if="project.javascript" src="@/assets/img/javascript.svg.png" alt="JavaScript" class="w-6 h-6" :title="'JavaScript'" />
-              <img v-if="project.Quasar" src="@/assets/img/Quasar.svg" alt="Quasar" class="w-6 h-6" :title="'Quasar'" />
-              <img v-if="project.node" src="@/assets/img/node.svg" alt="Node.js" class="w-6 h-6" :title="'Node.js'" />
+              <img v-if="project.firebase" src="@/assets/img/firebase.svg" alt="Firebase" class="w-6 h-6" title="Firebase" />
+              <img v-if="project.html" src="@/assets/img/html.svg" alt="HTML" class="w-6 h-6" title="HTML" />
+              <img v-if="project.vue" src="@/assets/img/vue-svgrepo-com.svg" alt="Vue" class="w-6 h-6" title="Vue.js" />
+              <img v-if="project.css" src="@/assets/img/css-3-svgrepo-com.svg" alt="CSS" class="w-6 h-6" title="CSS" />
+              <img v-if="project.tailwind" src="@/assets/img/tailwind.svg" alt="Tailwind" class="w-6 h-6" title="Tailwind" />
+              <img v-if="project.javascript" src="@/assets/img/javascript.svg.png" alt="JavaScript" class="w-6 h-6" title="JavaScript" />
+              <img v-if="project.Quasar" src="@/assets/img/Quasar.svg" alt="Quasar" class="w-6 h-6" title="Quasar" />
+              <img v-if="project.node" src="@/assets/img/node.svg" alt="Node.js" class="w-6 h-6" title="Node.js" />
+              <img v-if="project.node" src="@/assets/img/nuxt.png" alt="Nuxt.js" class="w-6 h-6" title="Nuxt.js" />
             </div>
             <a :href="project.github">
-              <img src="@/assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-6 h-6" :title="'GitHub - '" />
+              <img src="@/assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-6 h-6" title="GitHub" />
             </a>
           </div>
         </a>
@@ -42,8 +43,28 @@
 </template>
 
 <script setup>
-// const onde é adcionado sempre cada pojeto novo, lembre de olhar as props
 const projects = [
+  {
+    title: 'Feedel',
+    description: 'Loja de Vendas, em escala mundial.',
+    img: 'feedel.png',
+    vue: true,
+    Quasar: true,
+    node: true,
+    nuxt: true,
+    github: 'https://github.com/hellenictechnologies/feedel-dashboard-garbson',
+    link: 'https://feedel.app/login'
+  },
+  {
+    title: 'Dangerous Dragons',
+    description: 'Jogo RPG, feito com javasScript puro.',
+    img: 'dangerous.png',
+    html: true,
+    css: true,
+    javascript: true,
+    github: 'https://github.com/Garbson/Dangerous-dragon',
+    link: 'https://dangerous-dragon.pages.dev'
+  },
   {
     title: 'Hostel Pachacuteq',
     description: 'Hotel em Cusco, Peru, para descanso e aventuras.',
@@ -148,10 +169,10 @@ const projects = [
   }
 ]
 </script>
+
 <style scoped>
 .content {
   width: 72%;
-
   margin-bottom: 50px;
 }
 
