a5e73080ec0fb48b00811f29e5feb2ca0becee25
Garbson <garbsonsouza2602@gmail.com>
2025-03-27 13:25:36 -0300

    add projetos

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-[100%]">
-    <div class="content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
+    <div
+      class="content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto"
+    >
       <div
         v-for="project in projects"
         :key="project.title"
@@ -20,20 +22,95 @@
               <p class="description text-sm h-[100%] mt-2">{{ project.description }}</p>
             </div>
           </div>
-          <div class="linguagens-projeto flex justify-between items-center p-4 border-black mt-auto">
+          <div
+            class="linguagens-projeto flex justify-between items-center p-4 border-black mt-auto"
+          >
             <div class="flex space-x-2">
-              <img v-if="project.firebase" src="@/assets/img/firebase.svg" alt="Firebase" class="w-6 h-6" title="Firebase" />
-              <img v-if="project.html" src="@/assets/img/html.svg" alt="HTML" class="w-6 h-6" title="HTML" />
-              <img v-if="project.vue" src="@/assets/img/vue-svgrepo-com.svg" alt="Vue" class="w-6 h-6" title="Vue.js" />
-              <img v-if="project.css" src="@/assets/img/css-3-svgrepo-com.svg" alt="CSS" class="w-6 h-6" title="CSS" />
-              <img v-if="project.tailwind" src="@/assets/img/tailwind.svg" alt="Tailwind" class="w-6 h-6" title="Tailwind" />
-              <img v-if="project.javascript" src="@/assets/img/javascript.svg.png" alt="JavaScript" class="w-6 h-6" title="JavaScript" />
-              <img v-if="project.Quasar" src="@/assets/img/Quasar.svg" alt="Quasar" class="w-6 h-6" title="Quasar" />
-              <img v-if="project.node" src="@/assets/img/node.svg" alt="Node.js" class="w-6 h-6" title="Node.js" />
-              <img v-if="project.nuxt" src="@/assets/img/nuxt.png" alt="Nuxt.js" class="w-6 h-6" title="Nuxt.js" />
+              <img
+                v-if="project.firebase"
+                src="@/assets/img/firebase.svg"
+                alt="Firebase"
+                class="w-6 h-6"
+                title="Firebase"
+              />
+              <img
+                v-if="project.html"
+                src="@/assets/img/html.svg"
+                alt="HTML"
+                class="w-6 h-6"
+                title="HTML"
+              />
+              <img
+                v-if="project.vue"
+                src="@/assets/img/vue-svgrepo-com.svg"
+                alt="Vue"
+                class="w-6 h-6"
+                title="Vue.js"
+              />
+              <img
+                v-if="project.css"
+                src="@/assets/img/css-3-svgrepo-com.svg"
+                alt="CSS"
+                class="w-6 h-6"
+                title="CSS"
+              />
+              <img
+                v-if="project.tailwind"
+                src="@/assets/img/tailwind.svg"
+                alt="Tailwind"
+                class="w-6 h-6"
+                title="Tailwind"
+              />
+              <img
+                v-if="project.javascript"
+                src="@/assets/img/javascript.svg.png"
+                alt="JavaScript"
+                class="w-6 h-6"
+                title="JavaScript"
+              />
+              <img
+                v-if="project.typescript"
+                src="@/assets/img/typescript.png"
+                alt="typescript"
+                class="w-6 h-6"
+                title="typescript"
+              />
+              <img
+                v-if="project.Quasar"
+                src="@/assets/img/Quasar.svg"
+                alt="Quasar"
+                class="w-6 h-6"
+                title="Quasar"
+              />
+              <img
+                v-if="project.node"
+                src="@/assets/img/node.svg"
+                alt="Node.js"
+                class="w-6 h-6"
+                title="Node.js"
+              />
+              <img
+                v-if="project.nuxt"
+                src="@/assets/img/nuxt.png"
+                alt="Nuxt.js"
+                class="w-6 h-6"
+                title="Nuxt.js"
+              />
+              <img
+                v-if="project.bootstrap"
+                src="@/assets/img/bootstrap.png"
+                alt="bootstrap"
+                class="w-6 h-6"
+                title="bootstrap"
+              />
             </div>
             <a :href="project.github">
-              <img src="@/assets/img/github-142-svgrepo-com.svg" alt="GitHub" class="w-6 h-6" title="GitHub" />
+              <img
+                src="@/assets/img/github-142-svgrepo-com.svg"
+                alt="GitHub"
+                class="w-6 h-6"
+                title="GitHub"
+              />
             </a>
           </div>
         </a>
